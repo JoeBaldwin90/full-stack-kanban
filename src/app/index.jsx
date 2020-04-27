c9ad6fcb9ec19d8e0060cef7b52b26e906c56f5e
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-04-27 20:35:58 +0100

    React set-up and first component: Dashboard

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { store } from "./store";
+
+import { Dashboard } from "./components/Dashboard";
+
+console.log(store.getState());
+
+ReactDOM.render(<Dashboard />, document.getElementById("app"));
