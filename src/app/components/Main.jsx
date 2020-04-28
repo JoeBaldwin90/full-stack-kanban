2247fabd4309f509ee89ada2aaaa39db81283372
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-04-28 08:27:13 +0100

    Map State to Props for the Redux connected Dashboard component

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../store';
-import { Dashboard } from './Dashboard'
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import { ConnectedDashboard } from "./Dashboard";
 
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-            <Dashboard />
-        </div>
-    </Provider>
-)
+  <Provider store={store}>
+    <div>
+      <ConnectedDashboard />
+    </div>
+  </Provider>
+);
