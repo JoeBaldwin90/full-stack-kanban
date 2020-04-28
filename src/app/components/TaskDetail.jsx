1c1e935e0dafa4c83b8cc6cddef15819e5a50c46
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-04-28 18:24:03 +0100

    Map state to props for Taks Detail component

--- src/app/components/TaskDetail.jsx
+++ src/app/components/TaskDetail.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
-const TaskDetail = ({ id, comments, task, isComplete }) => (
-  <div>Task Detail</div>
+const TaskDetail = ({ id, task, isComplete, groups }) => (
+  <Fragment>
+    <div>
+      <input value={task.name} />
+    </div>
+    <div>
+      <button>Toggle Completed/Incomplete</button>
+    </div>
+
+    <div>
+      <select>
+        {groups.map((group) => (
+          <option key={group.id} value={group.id}>
+            {group.name}
+          </option>
+        ))}
+      </select>
+    </div>
+
+    <div>
+      <Link to='/dashboard'>
+        <button>Done</button>
+      </Link>
+    </div>
+  </Fragment>
 );
 
-const mapStateToProps = state => state;
+function mapStateToProps(state, ownProps) {
+  let id = ownProps.match.params.id;
+  let task = state.tasks.find(task => task.id === id);
+  let groups = state.groups;
+
+  return {
+    id,
+    task,
+    isComplete: task.isComplete,
+    groups,
+  };
+}
 
 export const ConnectedTaskDetail = connect(
   mapStateToProps
