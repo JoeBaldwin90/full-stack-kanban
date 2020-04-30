336434342a60aeaeb480a1a9d02bb34980ecad20
Joseph Baldwin <joseph.baldwin@synchronoss.com>
2020-04-30 18:46:02 +0100

    Add session to store and use a route guard

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -4,20 +4,29 @@ import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
 import { ConnectedNavigation } from "./Navigation";
 import { ConnectedTaskDetail } from "./TaskDetail";
-import { Router, Route } from "react-router";
+import { Router, Route,  Redirect  } from "react-router-dom";
 import { history } from "../store/history";
+
+const RouteGuard = Component => ({ match }) => {
+  console.info("Route Guard: ", match);
+  if (!store.getState().session.isAuthenticated) {
+    console.log("**** Not Authenticated ****")
+    return <Redirect to="/"/>
+  } else {
+    return <Component match={match} />
+  }
+}
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
       <div>
         <ConnectedNavigation />
-        <Route exact path='/dashboard' render={() => <ConnectedDashboard />} />
+        <Route exact path='/dashboard' render={RouteGuard(ConnectedDashboard)} />
         <Route
           exact
           path='/task/:id'
-          render={({ match }) => <ConnectedTaskDetail match={match} />}
-        />
+          render={RouteGuard(ConnectedTaskDetail)} />
       </div>
     </Provider>
   </Router>
