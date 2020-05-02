d914deac3d9083a2c95b0267657d5a2a9759f5ba
Joseph Baldwin <joseph.baldwin@synchronoss.com>
2020-05-02 15:06:35 +0100

    Create user-specific state on successful login

--- src/server/authenticate.js
+++ src/server/authenticate.js
@@ -1,15 +1,27 @@
 import md5 from 'md5';
 import { connectDB } from './connect-db';
 
+async function assembleUSerState(user) {
+  let db = await connectDB();
+  let tasks = db.collection('tasks').find({ owner: user.id }).toArray();
+  let groups = db.collection('groups').find({ owner: user.id }).toArray();
+
+  return {
+    tasks,
+    groups,
+    session: { authenticated: 'AUTHENTICATED', id: user.id }
+  }
+}
+
 export const authenticationRoute = app => {
   app.post("/authenticate", async (req, res) => {
     let { username, password } = req.body;
     let db = await connectDB();
     let collection = db.collection('users');
 
-    let user = await collection.findOne({name: username});
+    let user = await collection.findOne({ name: username });
 
-    if (!user) { 
+    if (!user) {
       return res.status(500).send("User not found");
     }
 
@@ -19,7 +31,9 @@ export const authenticationRoute = app => {
     if (!passwordCorrect) {
       return res.status(500).send("Incorrect password");
     }
+    let state = await assembleUSerState(user);
+    console.log(user)
 
-
+    res.send({ state });
   })
 }
