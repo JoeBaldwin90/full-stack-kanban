8770f373f17e2d333e0c388cc560708539d07591
Joseph Baldwin <joseph.baldwin@synchronoss.com>
2020-05-04 22:58:07 +0100

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        loader: "babel-loader",
-      },
-    ],
-  },
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
