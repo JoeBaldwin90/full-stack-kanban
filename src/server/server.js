3f8ce194970b8a19dac87d04f192eec6444a63c0
Joseph Baldwin <joseph.baldwin@synchronoss.com>
2020-05-04 20:51:33 +0100

    Heroku

--- src/server/server.js
+++ src/server/server.js
@@ -20,7 +20,7 @@ app.use(cors(), bodyParser.urlencoded({ extended: true }), bodyParser.json());
 authenticationRoute(app);
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.resolve(__dirname, '../../dist')))
+  app.use(express.static(path.resolve(__dirname, '../../dist/bundle.js')))
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve('index.html'))
   })
