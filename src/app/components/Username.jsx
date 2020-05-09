8b5f9acceec4378e94c6bae5d01f92dac7db4176
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-09 21:51:55 +0100

    Styled greeting component

--- src/app/components/Username.jsx
+++ src/app/components/Username.jsx
@@ -1,9 +1,19 @@
 import React, { Fragment } from "react";
+import styled, { css } from "styled-components";
+
+const Greeting = styled.p`
+  font-family: "IBM Plex Mono", monospace;
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid seagreen;
+  color: seagreen;
+  padding: 0.25em 1em;
+`;
 
 export const Username = ({ username }) => {
   return (
     <Fragment>
-      {username ? <h5>Welcome, {username}!</h5> : undefined}
+      {username && <Greeting>Welcome, {username}!</Greeting>}
     </Fragment>
-  )
+  );
 };
