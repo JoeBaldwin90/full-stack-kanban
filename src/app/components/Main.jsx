16cc674266b10482600d41d12a1bd625b57fa71a
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-09 21:52:58 +0100

    Global styles

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,35 +1,61 @@
-import React from "react";
-import { Router, Route, Redirect  } from "react-router-dom";
+import React, { Fragment } from "react";
+import { createGlobalStyle } from "styled-components";
+import { Normalize } from "styled-normalize";
+import { Router, Route, Redirect } from "react-router-dom";
 import { history } from "../store/history";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "../containers/ConnectedDashboard";
 import { ConnectedNavigation } from "../containers/ConnectedNavigation";
 import { ConnectedTaskDetail } from "../containers/ConnectedTaskDetail";
-import { ConnectedLogin } from '../containers/ConnectedLogin';
+import { ConnectedLogin } from "../containers/ConnectedLogin";
 
-const RouteGuard = Component => ({ match }) => {
+const GlobalStyles = createGlobalStyle`
+ html {
+   box-sizing: border-box;
+}
+
+body {
+  font-family: 'IBM Plex Mono', monospace;
+  font-weight: 300;
+}
+
+ *,
+ *::before,
+ *::after {
+   box-sizing: inherit;
+}
+`;
+
+const RouteGuard = (Component) => ({ match }) => {
   console.info("Route Guard: ", match);
   if (!store.getState().session.authenticated) {
-    console.log("**** Not Authenticated ****")
-    return <Redirect to="/login"/>
+    console.log("**** Not Authenticated ****");
+    return <Redirect to='/login' />;
   } else {
-    return <Component match={match} />
+    return <Component match={match} />;
   }
-}
+};
 
 export const Main = () => (
   <Router history={history}>
     <Provider store={store}>
-      <div>
+      <Fragment>
+        <Normalize />
+        <GlobalStyles />
         <ConnectedNavigation />
         <Route exact path='/login' component={ConnectedLogin} />
-        <Route exact path='/dashboard' render={RouteGuard(ConnectedDashboard)} />
+        <Route
+          exact
+          path='/dashboard'
+          render={RouteGuard(ConnectedDashboard)}
+        />
         <Route
           exact
           path='/task/:id'
-          render={RouteGuard(ConnectedTaskDetail)} />
-      </div>
+          render={RouteGuard(ConnectedTaskDetail)}
+        />
+      </Fragment>
     </Provider>
   </Router>
 );
