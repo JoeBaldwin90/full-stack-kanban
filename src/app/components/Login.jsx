f60caaf4fbac7620a6fd6dd86a599a5da489bd18
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-10 02:37:49 +0100

    Styled components login page

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -1,29 +1,95 @@
 import React, { Fragment } from "react";
-import * as mutations from '../store/mutations';
+import styled from "styled-components";
+import colours from "../styles/colours";
+import * as mutations from "../store/mutations";
+import {
+  Button,
+  Title,
+  WarningMessage,
+  Form,
+  FormInput,
+} from "../styles/shared.js";
 
-export const Login = ({ authenticateUser, authenticated, createUser, requestSignUp, noAccount }) => {
+const LoginGrid = styled.div`
+  grid-column: 2 / span 10;
+  border: solid 2px ${colours.navy};
+  padding: 1em;
+  border-radius: 1em;
+`;
+
+const ExposeSignUp = styled.span`
+  text-decoration: underline;
+  color: ${colours.blue};
+  cursor: pointer;
+  &:hover {
+    color: ${colours.pink};
+  }
+`;
+
+export const Login = ({
+  authenticateUser,
+  authenticated,
+  createUser,
+  requestSignUp,
+  noAccount,
+}) => {
   return (
-    <Fragment>
-      <h2>Please Log In</h2>
-      <form onSubmit={authenticateUser}>
-        <input type="text" placeholder="Username" name="username" defaultValue="Dev"></input>
-        <input type="password" placeholder="Password" name="password" defaultValue=""></input>
-        {authenticated === mutations.NOT_AUTHENTICATED ? <p>Incorrect logins</p> : null}
-        <button type="submit">Log-in</button>
-      </form>
-      {noAccount
-        ? <Fragment />
-        : (<Fragment>
-          <p>No account? <span><button onClick={requestSignUp}>Sign up!</button></span></p>
-        </Fragment>)}
-      {!noAccount
-        ? <Fragment />
-        : <form onSubmit={createUser}>
-          <input type="text" placeholder="Create username" name="username" defaultValue=""></input>
-          <input type="password" placeholder="Create password" name="password" defaultValue=""></input>
-          <button type="submit">Sign-Up!</button>
-        </form>
-      }
-    </Fragment>
-  )
+    <LoginGrid>
+      <Title>Please Log In</Title>
+      <Form onSubmit={authenticateUser}>
+        <FormInput
+          type='text'
+          placeholder='Username'
+          name='username'
+          defaultValue='Dev'
+        />
+        <FormInput
+          type='password'
+          placeholder='Password'
+          name='password'
+          defaultValue=''
+        />
+        <Button wide type='submit'>
+          Log-in
+        </Button>
+      </Form>
+
+      {authenticated === mutations.NOT_AUTHENTICATED ? (
+        <WarningMessage>* Incorrect logins</WarningMessage>
+      ) : null}
+
+      {noAccount ? (
+        <Fragment />
+      ) : (
+        <p>
+          <span>No account?</span>{" "}
+          <ExposeSignUp onClick={requestSignUp}>
+            Sign up!
+          </ExposeSignUp>
+        </p>
+      )}
+
+      {!noAccount ? (
+        <Fragment />
+      ) : (
+        <Form onSubmit={createUser}>
+          <FormInput
+            type='text'
+            placeholder='Create username'
+            name='username'
+            defaultValue=''
+          ></FormInput>
+          <FormInput
+            type='password'
+            placeholder='Create password'
+            name='password'
+            defaultValue=''
+          ></FormInput>
+          <Button wide type='submit'>
+            Sign-Up!
+          </Button>
+        </Form>
+      )}
+    </LoginGrid>
+  );
 };
